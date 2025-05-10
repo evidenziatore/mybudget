3e1009d39b45b0a9f910fc37d790b912b5a065af
evidenziatore <giulioforino@icloud.com>
2025-05-10 15:07:29 +0200

    menu a sinistra

--- src/main/java/com/evidenziatore/mybudget/controller/ControllerMyBudget.java
+++ src/main/java/com/evidenziatore/mybudget/controller/ControllerMyBudget.java
@@ -1,21 +1,68 @@
 package com.evidenziatore.mybudget.controller;
 
-import com.evidenziatore.mybudget.database.Database;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.collections.FXCollections;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class ControllerMyBudget implements Initializable {
+public class ControllerMyBudget {
 
     @FXML
-    private VBox mainVbox;
+    private VBox contentArea;
 
-    @Override
-    public void initialize(URL url, ResourceBundle caricatoreRisorse) {
-        Database.getAllMovimentiCompletamente();
+    @FXML
+    private ListView<String> menuListView;
+
+    @FXML
+    public void initialize() {
+        menuListView.setItems(FXCollections.observableArrayList(
+                "Movimenti",
+                "Prodotti",
+                "Categorie",
+                "Provenienze"
+        ));
+
+        // Seleziona "Movimenti" di default
+        menuListView.getSelectionModel().select(0);
+        showMovimenti();
+
+        // Listener cambiamento selezione menu
+        menuListView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                switch (newVal) {
+                    case "Movimenti" -> showMovimenti();
+                    case "Prodotti" -> showProdotti();
+                    case "Categorie" -> showCategorie();
+                    case "Provenienze" -> showProvenienze(); // Nuovo caso
+                }
+            }
+        });
+
+        menuListView.setFixedCellSize(32);
+        menuListView.prefHeightProperty().bind(
+                Bindings.size(menuListView.getItems()).multiply(menuListView.getFixedCellSize()+1)
+        );
     }
 
+    private void showMovimenti() {
+        contentArea.getChildren().clear();
+        contentArea.getChildren().add(new Label("Schermata Movimenti"));
+    }
+
+    private void showCategorie() {
+        contentArea.getChildren().clear();
+        contentArea.getChildren().add(new Label("Schermata Categorie"));
+    }
+
+    private void showProdotti() {
+        contentArea.getChildren().clear();
+        contentArea.getChildren().add(new Label("Schermata Prodotti"));
+    }
+
+    private void showProvenienze() {
+        contentArea.getChildren().clear();
+        contentArea.getChildren().add(new Label("Schermata Provenienze"));
+    }
 }
---
+++ src/main/java/com/evidenziatore/mybudget/controller/ControllerTabellaMovimenti.java
@@ -0,0 +1,63 @@
+package com.evidenziatore.mybudget.controller;
+
+import com.evidenziatore.mybudget.database.Database;
+import com.evidenziatore.mybudget.database.entity.Movimento;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class ControllerTabellaMovimenti {
+
+    @FXML
+    private TableView<Movimento> tableViewMovimenti;
+
+    @FXML
+    private TableColumn<Movimento, Integer> colId;
+
+    @FXML
+    private TableColumn<Movimento, String> colData;
+
+    @FXML
+    private TableColumn<Movimento, String> colTipologia;
+
+    @FXML
+    private TableColumn<Movimento, String> colCategoria;
+
+    @FXML
+    private TableColumn<Movimento, String> colImportanza;
+
+    @FXML
+    private TableColumn<Movimento, String> colProvenienza;
+
+    @FXML
+    private TableColumn<Movimento, String> colProdotto;
+
+    @FXML
+    public void initialize() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colData.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getData().toString()));
+        colTipologia.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getTipologia().getValore()));
+        colCategoria.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getCategoria().getValore()));
+        colImportanza.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getCategoria().getImportanza().getValore()));
+        colProvenienza.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getProvenienza().getValore()));
+        colProdotto.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getProdotto().getValore()));
+
+        double totalWidth = colId.getPrefWidth()
+                + colData.getPrefWidth()
+                + colTipologia.getPrefWidth()
+                + colCategoria.getPrefWidth()
+                + colImportanza.getPrefWidth()
+                + colProvenienza.getPrefWidth()
+                + colProdotto.getPrefWidth()
+                + 20;
+
+        tableViewMovimenti.setMaxWidth(totalWidth);
+
+        ObservableList<Movimento> movimentiList = FXCollections.observableArrayList(Database.getAllMovimentiCompletamente());
+
+        tableViewMovimenti.setItems(movimentiList);
+    }
+}
